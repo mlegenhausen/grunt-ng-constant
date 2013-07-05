403f7f3a5c0633268831e6d0b91924ed09308ca7
Gabriel Mancini <gabriel.mancini@gmail.com>
2013-07-05 13:52:19 -0300

    add feature of wrap add files to test

---
+++ test/expected/wrap_options.js
@@ -0,0 +1,12 @@
+define( ["angular", "ngResource", "ngCookies"], function() { 
+ return angular.module("module2", ["test"])
+
+.constant("constant1", {
+	"key1": 123,
+	"key2": "value2",
+	"foobar": false
+})
+
+; 
+
+});
